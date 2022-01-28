339e2babe40714f524ea5dce4b18f170ee3d2d2d
Fabrice Desré <fabrice@desre.org>
2022-01-27 20:42:21 -0800

    Update Android build paths

--- Android.mk
+++ Android.mk
@@ -13,22 +13,22 @@ LOCAL_MODULE_PATH  := $(TARGET_OUT)
 include $(BUILD_SYSTEM)/base_rules.mk
 
 B2G_DEFAULTS := $(abspath $(TARGET_OUT)/b2g/defaults)
-GAIA_PATH ?= gaia/ui
+GAIA_PATH ?= gaia
 
 $(LOCAL_INSTALLED_MODULE): $(LOCAL_BUILT_MODULE)
 	@echo "Installing frontend..."
 	# Copy the custom prefs file.
 	@mkdir -p $(B2G_DEFAULTS)/pref
-	@cp $(GAIA_PATH)/scripts/common.js $(B2G_DEFAULTS)/pref/common.js
-	@cp $(GAIA_PATH)/scripts/teracube.js $(B2G_DEFAULTS)/pref/teracube.js
+	@cp $(GAIA_PATH)/defaults/pref/common.js $(B2G_DEFAULTS)/pref/common.js
+	@cp $(GAIA_PATH)/defaults/pref/teracube.js $(B2G_DEFAULTS)/pref/teracube.js
 	# Copy the default settings
-	@cp $(GAIA_PATH)/scripts/default-settings.json $(B2G_DEFAULTS)/settings.json
+	@cp $(GAIA_PATH)/defaults/default-settings.json $(B2G_DEFAULTS)/settings.json
 	# Create a "buffer file" to have spare room on the system partition.
 	dd if=/dev/urandom of=$(B2G_DEFAULTS)/delete_me_when_disk_full bs=1M count=100
 
 $(LOCAL_BUILT_MODULE):
 	@echo "Packaging apps..."
 	(cd $(GAIA_PATH)/builder ; \
-	 CAPYLOON_APPS=$(abspath $(GAIA_PATH)/apps) \
+	 CAPYLOON_APPS_ROOT=$(abspath $(GAIA_PATH)/apps) \
          $(SHELL) install.sh $(B2G_DEFAULTS)/../)
 
