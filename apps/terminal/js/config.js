cbf4e71dbb313f9c620a4e4122098de4938ce861
Fabrice Desré <fabrice@desre.org>
2022-08-19 17:02:10 -0700

    [terminal] Switch to Shoelace

---
+++ apps/terminal/js/config.js
@@ -0,0 +1,36 @@
+// We run on the default port on device.
+const isDevice = location.port === "";
+
+window.config = {
+  isDevice,
+  port: isDevice ? 80 : 8081,
+};
+
+function addLink(url) {
+  let link = document.createElement("link");
+  link.setAttribute("rel", "stylesheet");
+  link.setAttribute("href", url);
+  document.head.appendChild(link);
+  return link;
+}
+
+function loadScript(url, defer = false) {
+  let script = document.createElement("script");
+  script.setAttribute("src", url);
+  if (defer) {
+    script.setAttribute("defer", "true");
+  }
+  document.head.appendChild(script);
+  return script;
+}
+
+function loadSharedScript(url) {
+  return loadScript(`http://shared.localhost:${location.port}/${url}`);
+}
+
+// Load <link rel="stylesheet" href="style/{device|desktop}.css" />
+addLink(`/style/${isDevice ? "device" : "desktop"}.css`);
+
+let depGraphLoaded = new Promise((resolve) => {
+  loadSharedScript("js/dep_graph.js").onload = resolve;
+});
