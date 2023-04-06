afc739c457a24987f4a44619f97747a3edb51c51
Fabrice Desré <fabrice@desre.org>
2023-04-05 18:16:30 -0700

    [tiles] fix typo

--- apps/tiles/resources/new-tile/sw.js
+++ apps/tiles/resources/new-tile/sw.js
@@ -7,7 +7,7 @@ const ACTIVITIES_URL = {
 try {
   importScripts(`http://shared.localhost/js/activity_sw.js`);
 } catch (e) {
-  // If we the load from port 80 fails, fallback to port 8081.
+  // If the load from port 80 fails, fallback to port 8081.
   // TODO: smarter detection of which port to use.
   importScripts(`http://shared.localhost:8081/js/activity_sw.js`);
 }
