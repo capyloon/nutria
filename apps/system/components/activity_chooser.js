764f72c61fd9a3915891808f73034dd50ae2f11f
Fabrice Desré <fabrice@desre.org>
2023-05-17 17:25:21 -0700

    [system] Re-write the activity chooser to workaround a Lit bug :(

--- apps/system/components/activity_chooser.js
+++ apps/system/components/activity_chooser.js
@@ -1,6 +1,8 @@
 // Represents an <activity-chooser> element
+// Not using LitElement to workaround a bug in Lit
+// when updating the l10n part.
 
-class ActivityChooser extends LitElement {
+class ActivityChooser extends HTMLElement {
   constructor() {
     super();
     this.drawer = this.parentElement;
@@ -8,20 +10,23 @@ class ActivityChooser extends LitElement {
       event.preventDefault();
       this.canceled();
     });
-  }
 
-  static get properties() {
-    return {
-      apps: { state: true },
-    };
-  }
-
-  updated() {
-    document.l10n.translateFragment(this.shadowRoot);
+    let shadow = this.attachShadow({ mode: "open" });
+    shadow.innerHTML = `<div class="container">
+      <link rel="stylesheet" href="components/activity_chooser.css" />
+      <h3></h3>
+      <sl-menu></sl-menu>
+      <sl-divider></sl-divider>
+      <sl-button
+        variant="neutral"
+        data-l10n-id="button-cancel"
+      ></sl-button>
+    </div>`;
+    shadow.querySelector("sl-button").onclick = this.canceled.bind(this);
   }
 
   async open(providers) {
-    console.log(`ABCD ${JSON.stringify(providers)}`);
+    console.log(`activity_chooser: ${JSON.stringify(providers)}`);
     let apps = [];
     for (let item of providers?.choices) {
       let summary = await window.appsManager.getSummary({
@@ -33,10 +38,21 @@ class ActivityChooser extends LitElement {
     this.activityId = providers.id;
     this.apps = apps;
     this.activityName = providers.name;
+
+    this.render();
+
     this.drawer.show();
+
     return new Promise((resolve, reject) => {
       this.deferred = { resolve, reject };
     });
+  }
+
+  reset() {
+    this.deferred = null;
+    this.providers = null;
+    this.apps = null;
+    this.render();
   }
 
   makeChoice(event) {
@@ -45,8 +61,7 @@ class ActivityChooser extends LitElement {
       "sl-after-hide",
       () => {
         this.deferred?.resolve({ id: this.activityId, value });
-        this.deferred = null;
-        this.providers = null;
+        this.reset();
       },
       { once: true }
     );
@@ -54,13 +69,12 @@ class ActivityChooser extends LitElement {
   }
 
   canceled() {
-    console.log(`ZZZ canceled`);
+    console.log(`activity_chooser: canceled`);
     this.drawer.addEventListener(
       "sl-after-hide",
       () => {
         this.deferred?.resolve({ id: this.activityId });
-        this.deferred = null;
-        this.providers = null;
+        this.reset();
       },
       { once: true }
     );
@@ -68,25 +82,29 @@ class ActivityChooser extends LitElement {
   }
 
   render() {
-    return html`<div class="container">
-      <link rel="stylesheet" href="components/activity_chooser.css" />
-      <h3 data-l10n-id="activity-${this.activityName}">${this.activityName}</h3>
-      <sl-menu>
-        ${this.apps?.map((app, index) => {
-          return html`<sl-menu-item
-            @click="${this.makeChoice}"
-            data-index=${index}
-            ><img src="${app.icon}" slot="prefix" />${app.title}</sl-menu-item
-          >`;
-        })}
-      </sl-menu>
-      <sl-divider></sl-divider>
-      <sl-button
-        variant="neutral"
-        data-l10n-id="button-cancel"
-        @click="${this.canceled}"
-      ></sl-button>
-    </div>`;
+    // console.log(`activity_chooser: activity_chooser render() ${this.activityName}`);
+    let menu = this.shadowRoot.querySelector("sl-menu");
+
+    menu.innerHTML = "";
+    let choiceFn = this.makeChoice.bind(this);
+    this.apps?.forEach((app, index) => {
+      let item = document.createElement("sl-menu-item");
+      item.dataset.index = index;
+      item.onclick = choiceFn;
+      item.textContent = app.title;
+
+      let icon = document.createElement("img");
+      icon.setAttribute("slot", "prefix");
+      icon.src = app.icon;
+      item.append(icon);
+
+      menu.append(item);
+    });
+
+    let title = this.shadowRoot.querySelector("h3");
+    title.innerText = this.activityName;
+    title.dataset.l10nId = `activity-${this.activityName}`;
+    document.l10n.translateFragment(this.shadowRoot);
   }
 }
 
