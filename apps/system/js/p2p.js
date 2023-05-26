9cf0f077272864b924f3652d47c303168b26027b
Fabrice Desré <fabrice@desre.org>
2023-05-26 12:19:30 -0700

    wip

--- apps/system/js/p2p.js
+++ apps/system/js/p2p.js
@@ -331,9 +331,6 @@ class P2pDiscovery {
           return this.controlId;
         }
 
-        console.log(
-          `this.controlId=${this.controlId} params.controlId=${params.controlId}`
-        );
         if (
           !params.controlId ||
           !this.controlId ||
@@ -344,17 +341,30 @@ class P2pDiscovery {
         }
 
         if (params.keypress) {
-          // Dispatch the keypress event to the active frame.
+          // Dispatch the keypress events to the active frame.
           let keys = params.keypress.split(",");
           let win = window.wm.currentFrame().webView.ownerGlobal;
-          let keg = new win.KeyboardEventGenerator();
-          keys.forEach((key) => {
-            keg.generate(new win.KeyboardEvent("keydown", { key }));
-          });
 
-          keys.reverse().forEach((key) => {
-            keg.generate(new win.KeyboardEvent("keyup", { key }));
-          });
+          // We can't use the KeyEventGenerator API because it doesn't support
+          // key sequences that change modifier states.
+          // Modifier state changes are needed eg. for [Shift]+[Tab] generation.
+          try {
+            let tip = Cc["@mozilla.org/text-input-processor;1"].createInstance(
+              Ci.nsITextInputProcessor
+            );
+            tip.beginInputTransaction(win, () => {});
+            keys.forEach((key) => {
+              tip.keydown(new win.KeyboardEvent("keydown", { key }));
+            });
+
+            keys.reverse().forEach((key) => {
+              tip.keyup(new win.KeyboardEvent("keyup", { key }));
+            });
+
+            tip = null;
+          } catch (e) {
+            console.error(`Failed to generate key event: ${e}`);
+          }
         }
         return true;
       }
