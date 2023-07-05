79a679e18dfec4dd240b081f09852b0e6a4d84b0
Fabrice Desré <fabrice@desre.org>
2023-07-05 12:36:53 -0700

    [debian] Update deb builder script to create Rpi package

--- build-debs.sh
+++ build-debs.sh
@@ -8,14 +8,19 @@ set -e
 ./jackady deb --device desktop
 cp ./builder/output/debian/*.deb .
 
-# Build the aarch64 debs for PinePhone and Librem5
+# Build the aarch64 debs for PinePhone, Librem5 and Rpi
 ./jackady update-prebuilts --target aarch64-unknown-linux-gnu
+
 ./jackady clean
 ./jackady deb --device pinephone
 cp ./builder/output/debian/*.deb .
 
 ./jackady clean
 ./jackady deb --device librem5
+cp ./builder/output/debian/*.deb .
+
+./jackady clean
+./jackady deb --device rpi
 cp ./builder/output/debian/*.deb .
 
 # Switch back to default prebuilts.
