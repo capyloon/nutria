1f41b12ece22698c50460283511ab93dd93d4c22
Fabrice Desré <fabrice@desre.org>
2023-07-18 00:52:08 -0700

    [l10n] Don't import gecko toolkit l10n anymore It's picked up by gecko now that the l10n manifest is packaged

--- update-gecko-l10n.sh
+++ update-gecko-l10n.sh
@@ -9,11 +9,5 @@ fi
 
 APP_ROOT=apps/gecko-l10n/locales/en-US
 
-# All the about: pages fluent resources.
-cp -R ${GECKO_PATH}/toolkit/locales/en-US/toolkit/about ${APP_ROOT}/toolkit/
-
-# Video controls
-cp ${GECKO_PATH}/toolkit/locales/en-US/toolkit/global/videocontrols.ftl ${APP_ROOT}/toolkit/global/
-
-# Various
+# browser files that are needed because they are referenced by toolkit/ :(
 cp ${GECKO_PATH}/browser/locales/en-US/browser/appExtensionFields.ftl ${APP_ROOT}/browser/appExtensionFields.ftl
